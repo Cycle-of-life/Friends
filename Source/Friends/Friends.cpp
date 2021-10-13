9610ad57e818187e28d809da697a89907b106a8f
Anastasia Ionicioiu <33623+antanaris@users.noreply.github.com>
2021-10-13 15:52:52 +0300

    Filtering sessions
    with AdvancedSessions plugin

---
+++ Source/Friends/Friends.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Friends.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Friends, "Friends" );
---
+++ Source/Friends/MyBlankClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBlankClass.h"
+
+MyBlankClass::MyBlankClass()
+{
+}
+
+MyBlankClass::~MyBlankClass()
+{
+}
